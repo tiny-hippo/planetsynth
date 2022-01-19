3c2f3d959b8ee632c6f53d996b43fc0dc72cebac
Simon <simonmueller.ch@gmail.com>
2022-01-19 10:30:24 +0100

    update support modules
    minor cosmetic changes

--- planetsynth/support/functions.py
+++ planetsynth/support/functions.py
@@ -1,43 +1,43 @@
-""" This module contains some useful support functions. """
 import numpy as np
+from numpy.typing import ArrayLike
 from .constants import sigma_b
 
 
-def get_L(R, Teff) -> np.ndarray:
+def get_L(R: ArrayLike, Teff: ArrayLike) -> np.ndarray:
     """Calculates the luminosity of a black body given its
     radius and effective temperature.
 
     Args:
-        R (array_like): The radius in cm.
-        Teff (array_like): The effective temperature in K.
+        R (ArrayLike): The radius in cm.
+        Teff (ArrayLike): The effective temperature in K.
 
     Returns:
-        array_like: The luminosity of a black body in erg/s.
+        ArrayLike: The luminosity of a black body in erg/s.
     """
     return 4 * np.pi * sigma_b * Teff ** 4 * R ** 2
 
 
-def get_R(L, Teff) -> np.ndarray:
+def get_R(L: ArrayLike, Teff: ArrayLike) -> np.ndarray:
     """Calculates the radius of a black body given its
     luminosity and effective temperature.
 
     Args:
-        L (array_like): The luminosity in cgs.
-        Teff (array_like): The effective temperature in K.
+        L (ArrayLike): The luminosity in cgs.
+        Teff (ArrayLike): The effective temperature in K.
 
     Returns:
-        array_like: The radius of a black body in cm.
+        ArrayLike: The radius of a black body in cm.
     """
     return np.sqrt(L / (4 * np.pi * sigma_b * Teff ** 4))
 
 
-def get_F(L, d) -> np.ndarray:
+def get_F(L: ArrayLike, d: ArrayLike) -> np.ndarray:
     """Calculates the incident day-side flux given the stellar luminosity L
     and planetary semi-major axis d.
 
     Args:
-        L (array_like): Stellar luminosity in erg/s.
-        d (array_like): Planetary semi-major axis in cm.
+        L (ArrayLike): Stellar luminosity in erg/s.
+        d (ArrayLike): Planetary semi-major axis in cm.
 
     Returns:
         np.ndarray: The incident day-side flux in erg/s/cm2
@@ -45,15 +45,15 @@ def get_F(L, d) -> np.ndarray:
     return L / (4 * np.pi * d ** 2)
 
 
-def get_Teq_from_L(L, d, A) -> np.ndarray:
+def get_Teq_from_L(L: ArrayLike, d: ArrayLike, A: ArrayLike) -> np.ndarray:
     """Calculates the equilibrium temperature of a planet
     given the stellar luminosity L, planetary semi-major axis d
     and surface albedo A:
 
     Args:
-        L (array_like): Stellar luminosity in erg/s.
-        d (array_like): Planetary semi-major axis in cm.
-        A (array_like): Planetary albedo.
+        L (ArrayLike): Stellar luminosity in erg/s.
+        d (ArrayLike): Planetary semi-major axis in cm.
+        A (ArrayLike): Planetary albedo.
 
     Returns:
         np.ndarray: The planetary equilibrium temperature in K.
@@ -61,16 +61,18 @@ def get_Teq_from_L(L, d, A) -> np.ndarray:
     return ((L * (1 - A)) / (16 * sigma_b * np.pi * d ** 2)) ** 0.25
 
 
-def get_Teq_from_T(T, R, d, A) -> np.ndarray:
+def get_Teq_from_T(
+    T: ArrayLike, R: ArrayLike, d: ArrayLike, A: ArrayLike
+) -> np.ndarray:
     """Calculates the equilibrium temperature of a planet
     given the stellar effective temperature and temperature,
     planetary semi-major axis d and surface albedo A:
 
     Args:
-        T (array_like): Stellar effective temperature in K.
-        R (array_like): Stellar radius in cm.
-        d (array_like): Planetary semi-major axis in cm.
-        A (array_like): Planetary albedo.
+        T (ArrayLike): Stellar effective temperature in K.
+        R (ArrayLike): Stellar radius in cm.
+        d (ArrayLike): Planetary semi-major axis in cm.
+        A (ArrayLike): Planetary albedo.
 
     Returns:
         np.ndarray: The planetary equilibrium temperature in K.
