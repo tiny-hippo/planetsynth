58cc69b376c40e17d9dd17af586967e71fcb7c92
Simon <simonmueller.ch@gmail.com>
2022-01-24 08:25:36 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url="",
     author="Simon Müller",
     author_email="simon.mueller7@uzh.ch",
-    licence="MIT",
+    license="MIT",
     packages=find_packages(include=["planetsynth", "planetsynth.*"]),
     package_data={"planetsynth": ["interpolators/*.zip*"]},
     install_requires=["numpy", "scipy"],
